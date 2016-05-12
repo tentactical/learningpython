eda97ca3b8f5a169eda7a19571cbe76b08d38e27
tentactical <trevaellison@gmail.com>
2016-05-12 19:41:24 -0400

    where i got so far
    this is where i got so far. i have a few questions
    
    1: the search results with all faculty emails are stretched across 31 results pages so each url I would want python to 'read' has the same general address except the page number changes at the end (http://dartmouth.edu/faculty-directory/department?dept=All&page=(1-31) ). Is there any way to tell python to keep looking under the same general domain with a variable ending, until there's no more pages, without knowing the total number of pages? In this case I know there are 31 pages, so its a little easier? This is so i don't have to type url = http://dartmouth.edu/faculty-directory/department?dept=All&page=(1-31) the program isn't dependent on knowing the number of result pages. Is there a way to tell python to keep loading the general url: "http://dartmouth.edu/faculty-directory/department?dept=All&page=" for an unknown number of pages until there are no more pages?

--- demails.py
+++ demails.py
@@ -1,18 +1,25 @@
-#load proper libraries
 import requests
 import urllib
+import json
 from bs4 import BeautifulSoup
 
-#tell python to read the faculty directory page
 urls = ("http://dartmouth.edu/faculty-directory/department?dept=All&page=1", "http://dartmouth.edu/faculty-directory/department?dept=All")
 url = requests.get(urls)
 
-#organize and read the content
 soup = BeautifulSoup(url.content)
-g_data = soup.find_all("div", {"class": "content"})
 
-#print each entry
-for item in g_data:
-	print item.text
-url = requests.get(urls)
+name = soup.find_all("a", {"href":"/faculty-directory/"})
 
+title = soup.find_all("li", {"class":"first last"})
+
+email = soup.find_all("a","href":"mailto:")
+
+for item in name:
+	print name.text
+
+for item in title:
+	print title.text
+
+for item in email:
+	print email.text
+	
